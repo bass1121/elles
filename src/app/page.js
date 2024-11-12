233d22ae4795cd515fa75664361b3c369108f0a1
Sean Bass <seanbass99@gmail.com>
2024-11-11 19:41:26 -0600

    create login page, login css and subcounter component skeletons

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import GetPlaylist from "./getPlaylist/page.js";
 import Footer from "./footer/page.js";
 
-import { Logo, Homepic } from "./helpers/imageExport.js";
+import { Logo } from "./helpers/imageExport.js";
 
 
 export default function Home() {
@@ -11,24 +11,31 @@ export default function Home() {
         <nav className="navbar">
           <div className="nav-link-container">
             <div className="nav-link">
-              <p>Merchandise</p>
+              <a>Login</a>
             </div>
             <div className="nav-link">
-              <p>About Me</p>
-            </div>
-            <Image src={Logo} className="logo" alt="" />
-            <div className="nav-link">
-              <p>Login</p>
+              <a>Create Account</a>
             </div>
             <div className="nav-link">
-              <p>HyperLink</p>
+              <a>Merchandise</a>
+            </div>
+            <div className="nav-link">
+              <a>About Elle</a>
+            </div>
+            <div className="nav-link">
+              <a>Elle's Tiktok</a>
             </div>
           </div>
         </nav>
         <div className='home-wrapper'>
           <div className="home-text-wrapper">
+            <Image src={Logo} className="logo" alt="" />
             <h1>A Community For Trending Topics and Entertainment News</h1>
-            
+            <p>
+              Elle Bee is a content creator that delivers daily content on entertainment news, trending topics and true crime. Her community is an open community
+              that provides a space for her viewers to give their opinions and to discuss the topic. She encourages her members to join her blog and to give their 
+              opinion and/or start a blog of their own.
+            </p>           
           </div>
           <GetPlaylist />
         </div>
---
+++ src/app/subCounters/page.js
@@ -0,0 +1,7 @@
+export default function SubCounter() {
+  return (
+    <div className="counterContainer">
+      
+    </div>
+  )
+}
