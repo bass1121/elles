7ede5d90c7046a43ac63a953a2176793e013fac8
Sean Bass <seanbass99@gmail.com>
2024-11-15 19:16:44 -0600

    added login form slider attribute

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,46 +1,50 @@
 "use client";
-
+import { useEffect } from 'react';
+import Image from "next/image";
 import "./login.css";
 import { BiSolidLeftArrowSquare } from "react-icons/bi";
 import { BiSolidRightArrowSquare } from "react-icons/bi";
+import { Logo } from '../helpers/imageExport';
 
 export default function Login() {
-  const signUpButton = document.getElementById('signUp');
-  const signInButton = document.getElementById('signIn');
-  const container = document.getElementById('container');
+  useEffect(() => {
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
+    
+    if (signUpButton) {
+      signUpButton.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+      });
+    }
   
-  signUpButton.addEventListener('click', () => {
-    container.classList.add("right-panel-active");
-  });
-  
-  signInButton.addEventListener('click', () => {
-    container.classList.remove("right-panel-active");
-  });
-
+    if (signUpButton) {
+      signInButton.addEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+      }); 
+    }
+  }, []);
 
   return(
     <div className="loginPage-wrapper">
-
-      <div className="container" id="container">
-        
+      <div className="container" id="container">       
         <div className="form-container sign-up-container">
-          <form action="#">
+          <form action="#" id="sign-up-form">
+            <Image src={Logo} className="logo" alt="logo" />
             <h1>Create Account</h1>
-            <span>or use your email for registration</span>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <input id="sign-up-name" type="text" placeholder="Name" />
+            <input id="sign-up-email" type="email" placeholder="Email" />
+            <input id="sign-up-password" type="password" placeholder="Password" />
             <button>Sign Up</button>
           </form>
         </div>
     
-    
         <div className="form-container sign-in-container">
-          <form action="#">
+          <form action="#" id="sign-in-form">
+            <Image src={Logo} className="logo" alt="logo" priority/>
             <h1>Sign in</h1>
-            <span>or use your account</span>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <input id="sign-in-email" type="email" placeholder="Email" />
+            <input id="sign-in-password" type="password" placeholder="Password" />
             <a href="#">Forgot your password?</a>
             <button>Sign In</button>
           </form>
@@ -50,14 +54,14 @@ export default function Login() {
         <div className="overlay-container">
           <div className="overlay">
             <div className="overlay-panel overlay-left">
-              <h1>Welcome Back!</h1>
-              <p>To keep connected with us please login with your personal info</p>
-              <button className="ghost" id="signIn">Sign In</button>
+              <h1>Hello, Friend</h1>
+              <p>Already have an account?</p>
+              <button className="ghost" id="signIn"><BiSolidRightArrowSquare className='arrow-icon'/></button>
             </div>
             <div className="overlay-panel overlay-right">
-              <h1>Hello, Friend!</h1>
-              <p>Enter your personal details and start journey with us</p>
-              <button className="ghost" id="signUp">Sign Up</button>
+              <h1>Welcome Back!</h1>
+              <p>Need to sign up?</p>
+              <button className="ghost" id="signUp"><BiSolidLeftArrowSquare className='arrow-icon' /></button>
             </div>
           </div>
         </div>
